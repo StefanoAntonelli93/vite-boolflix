a0f65263d165daf52131876bc49f2cb26db6ae85
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-05 21:33:20 +0200

    [UPD] install bootstrap and axios

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Default scaffolding with sass in "devDependencies".
 ## for preview
 
 ctrl+shift+v
+
+# CHIAVE API
+
+415477f3fde7b23ef241d1d552d9ae84
--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-  New Project
+  <div class="container py-5">
+    <h1 class="text-danger">boolflix
+
+    </h1>
+  </div>
+
 </template>
 
 <style scoped></style>
