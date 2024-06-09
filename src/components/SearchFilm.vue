66a4739473850b2a0d5b32e26dfa21b93b218c9e
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-09 23:02:08 +0200

    [UPD] add overlay alle card + scrollbar alla trama

--- src/components/SearchFilm.vue
+++ src/components/SearchFilm.vue
@@ -32,52 +32,46 @@ export default {
 
         </div>
     </header>
-<div class="p-5 bg-secondary d-flex flex-wrap justify-content-between text-white  ">
+<div class="p-5 d-flex flex-wrap justify-content-between">
     <!-- FILM -->
-    <div v-for="(film, index) in store.film.results" :key="index" class="card">
+    <div v-for="(film, index) in store.film.results" :key="index" >
+   
        
-            <div  class="background-card":style="{ backgroundImage: `url(${this.store.apiInfo.poster}${this.store.apiInfo.sizePoster}${film.poster_path})` }">
-                <div class="content-card">
-
-                     <!-- poster -->
-                     <div>
-                         <!-- interpolazione poster$sizeposter$linkapi
-                         <img :src="`${this.store.apiInfo.poster}${this.store.apiInfo.sizePoster}${film.poster_path}`"
-                             :alt="film.title"> -->
-                     </div>
-             
-                     <p v-if="film.title === film.original_title">
-                         Titolo: {{ film.title }}</p>
-                     <p v-else>
-                     <p> Titolo: {{ film.title }}</p>
-                     <p> Titolo originale: {{ film.original_title }}</p>
-                     </p>
-                     <p>Lingua:
-                         <span v-if="film.original_language === 'it'">
-                             <img class="img_flags" src="/images/flags/italy.png">
-                         </span>
-                         <span v-else-if="film.original_language === 'en'">
-                             <img class="img_flags" src="/images/flags/united-kingdom.png">
-                             <img class="img_flags" src="/images/flags/united-states.png">
-                         </span>
-                         <span v-else-if="film.original_language === 'ja'">
-                             <img class="img_flags" src="/images/flags/japan.png">
-             
-                         </span>
-                         <span v-else>
-                             {{ film.original_language }}
-                         </span>
-                     </p>
-                         <!-- star rating -->
-                     <p>Voto: <StarRating :vote="film.vote_average" /> <span> ({{ film.vote_average }})</span></p>
-                 
-                    
-                     <p>Trama: {{ film.overview }}</p>
+            <div class="image-container mb-5">
+                <!-- poster -->
+                <img :src="`${this.store.apiInfo.poster}${this.store.apiInfo.sizePoster}${film.poster_path}`" :alt="film.title" class="poster-image">
+                
+                <!-- overlay content -->
+                <div class="overlay-content">
+                    <p v-if="film.title === film.original_title">
+                        Titolo: {{ film.title }}</p>
+                    <p v-else>
+                        <p> Titolo: {{ film.title }}</p>
+                        <p> Titolo originale: {{ film.original_title }}</p>
+                    </p>
+                    <p>Lingua:
+                        <span v-if="film.original_language === 'it'">
+                            <img class="img_flags" src="/images/flags/italy.png">
+                        </span>
+                        <span v-else-if="film.original_language === 'en'">
+                            <img class="img_flags" src="/images/flags/united-kingdom.png">
+                            <img class="img_flags" src="/images/flags/united-states.png">
+                        </span>
+                        <span v-else-if="film.original_language === 'ja'">
+                            <img class="img_flags" src="/images/flags/japan.png">
+                        </span>
+                        <span v-else>
+                            {{ film.original_language }}
+                        </span>
+                    </p>
+                    <!-- star rating -->
+                    <p>Voto: <StarRating :vote="film.vote_average" /> <span> ({{ film.vote_average }})</span></p>
+                    <p class="text-overflow" >Trama: {{ film.overview }}</p>
                 </div>
-
             </div>
-
-    </div>
+        
+   
+</div>
 
     <!-- SERIE TV  -->
     <div v-for="(tv, index) in store.tv.results" :key="index">
@@ -124,18 +118,33 @@ export default {
     width: 1rem;
     margin-right: 5px;
 }
-.card {
-    width:calc(100% / 4);
-    border: 3px solid gold;
-}
-.background-card {
-    width: 100%;
-    background-repeat: no-repeat;
-    background-size: cover;
-    padding: 10px;
-} .content-card {
 
+.image-container {
+    position: relative;
+   
+    }.poster-image {
+            width: 100%; 
+        
+        }
+        .overlay-content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: white; /* Colore del testo, cambia se necessario */
+    background-color: rgba(0, 0, 0, 0.8); /* Sfondo semi-trasparente, cambia se necessario */
+    padding: 20px; /* Spazio interno del contenitore, cambia se necessario */
+    width: 100%;
+    height: 100%;
+    opacity: 0; 
+}.image-container:hover .overlay-content {
+    opacity: 1; /* Mostra il contenuto quando si passa sopra con il mouse */
+}.text-overflow {
+    max-height: 200px;
+    overflow-y: auto;
+    margin-top: 2rem;
 }
+
+
 
 
 </style>
